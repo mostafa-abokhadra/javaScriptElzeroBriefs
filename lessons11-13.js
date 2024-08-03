2f2c39a3eefb746158feb9983fa585c6ac6e03e7
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-04 02:51:36 +0300

    converting lessons files to js files

---
+++ lessons11-13.js
@@ -0,0 +1,22 @@
+// variables
+// use it to store some value that you will be using repeatedly e:g website name
+
+var name = "mostafa", age = 22
+/*  when you create an html element and give it an id,\
+    the id name is gonna be a global variable that you can\
+    access in you js file or from any where in the system e:g
+*/
+<div id="div-id">bokhadra</div>
+//now in your js file
+console.log(div-id) //bokhadra
+
+// you can use the dollar sign in anywhere in your variable
+var $dollar$var$ = "$$$"
+
+// use camel case  to declare the variables 
+var userName, myHomeAddress;
+
+// use "let" and "const" to declare your vars instead of var as it has a lot of problems
+let name = "mostafa"
+let age = 21
+const team = "barcelona💙💖"
