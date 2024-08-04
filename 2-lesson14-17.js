73e334876e83ef40644218a20c4f12a23bfef4e1
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-04 03:06:24 +0300

    lesson 14 added

---
+++ 2-lesson14-17.js
@@ -0,0 +1,10 @@
+// you can put single quotes inside a string that is made using double quotes and vice verca without any requirement from you to use escape character
+console.log("mostafa 'abokhadra'")
+console.log('mostafa "abokhadra"')
+// otherwise use escape character backslash'\'
+console.log("mostafa \\	\"abokhadra\"") // mostafa \ "abokhadra"
+console.log("mostafa \
+			abokadra \
+			10.") // in case if you want to fromat your code you can also use the
+				  // the escape character.
+console.log("mostafa\nabokhadra\n"); // to add new lines
