a46cc4eb3264f31388b32d7fdd8dfdace82b5eab
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-04 03:23:52 +0300

    lesson 16 added

--- 2-lesson14-17.js
+++ 2-lesson14-17.js
@@ -13,3 +13,18 @@ console.log("mostafa\nabokhadra\n"); // to add new lines
 let firstName = "mostafa", lastName = "abokhdara"
 console.log(firstName + " " + lastName) //mostafa abokhadra
 console.log(firstName, lastName) // mostafa abokhadra
+
+// Template literal strings using backtick ``
+console.log(`${firstName} "" // ${lastName}`); // you don't have to scape single \
+                                              // or double quotess but you have to escape backslash operator
+console.log(`${firstName}
+${lastName}`); // this will actually print it in the output in new line
+// so you can actually create a whole html element easily using backtick
+
+let myElement = `
+	<div>
+		<h2>${firstName}<h2>
+		<h3>${lastName}<h3>
+	</div>
+`;
+document.write(myElement)
