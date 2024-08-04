c2363826efb2ff62d78c8a34bb2b57ca2540730f
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-04 04:02:54 +0300

    adding lesson 18

---
+++ 4-lessons18-20.js
@@ -0,0 +1,8 @@
+// if you tried to do some arithmetic operation between a string and a numbre 
+//regardless the + operator that act as concatenation operator
+// you will get an error of NaN (not a number)
+console.log(10 - "string") // NaN 
+console.log(10 * "string") // NaN 
+// funny fact: typeof NaN is number :)
+console.log(2 ** 4) // 16 which is 2 * 2 * 2 * 2
+
